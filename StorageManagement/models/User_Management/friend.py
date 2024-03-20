0f7206f392848e78f4c2cded179bb1a345ed3ba3
Dada Clement Authority <dadaauthourity23@gmail.com>
2024-03-20 11:25:10 +0100

    friendship system is now tested and a few errors were fixed

--- StorageManagement/models/User_Management/friend.py
+++ StorageManagement/models/User_Management/friend.py
@@ -1,14 +1,18 @@
 from sqlalchemy import Column, String, ForeignKey, Enum
 
-from databases.basemodel_1 import Base, BaseModel
+from ..databases.basemodel_1 import Base, BaseModel
 
 
 class Friend(BaseModel, Base):
-    friend_id = Column(String(150), ForeignKey('user.id'), nullable=False)
-    user_id = Column(String(150), ForeignKey('user.id'), nullable=False)
+    __tablename__ = 'friends'
+
+    friend_id = Column(String(150), ForeignKey('users.id'), nullable=False)
+    user_id = Column(String(150), ForeignKey('users.id'), nullable=False)
     status = Column(Enum("pending", "friends", "blocked"), nullable=False)
 
     def __init__(self, friend_id, user_id, status):
+        super().__init__()
+
         self.friend_id = friend_id
         self.user_id = user_id
         self.status = status
--- StorageManagement/views/usermanagement/friend.py
+++ StorageManagement/views/usermanagement/friend.py
@@ -32,24 +32,26 @@ def accept_friend():
     if friend_id is None:
         return jsonify({'msg': 'friend_id is not present in request'}), 400
     
-    friend = Friend.search(user_id=user_id, friend_id=friend_id)
+    friend = Friend.search(user_id=friend_id, friend_id=user_id)
     if friend is None:
         return jsonify({'msg': 'Friend request not found'}), 404
-    friend[0].status = "friends"
-    friend[0].save()
+    
+    if friend[0].status == "pending":
+        friend[0].status = "friends"
+        friend[0].save()
 
     return jsonify({"msg": "The friend has been accepted successfully"}), 201
 
-@bp.route('/remove_friend', methods=['POST'], strict_slashes=False)
+@bp.route('/reject_friend_request', methods=['POST'], strict_slashes=False)
 @jwt_required()
-def remove_friend():
+def reject_friend_request():
     user_id = get_jwt_identity()['id']
     friend_id = request.json.get('friend_id', None)
     
     if friend_id is None:
         return jsonify({'msg': 'friend_id is not present in request'}), 400
     
-    friend = Friend.search(user_id=user_id, friend_id=friend_id)
+    friend = Friend.search(user_id=friend_id, friend_id=user_id)
     if friend is None:
         return jsonify({'msg': 'Friend not found'}), 404
     friend[0].delete()
@@ -59,13 +61,24 @@ def remove_friend():
 @bp.route('/friends', methods=['GET'], strict_slashes=False)
 @jwt_required()
 def get_friends():
+    no_friends = False
     user_id = get_jwt_identity()['id']
+
     friends = Friend.search(user_id=user_id, status="friends")
     if friends is None:
-        return jsonify({'msg': 'No friends found'}), 404
+        no_friends = True
+
     list_of_friends = []
     for friend in friends:
         list_of_friends.append(friend.to_dict())
+
+    friends = Friend.search(friend_id=user_id, status="Friends")
+    if friends is None and no_friends:
+        return jsonify({'msg': 'No friends found'}), 404
+    else:
+        for friend in friends:
+            list_of_friends.append(friend.to_dict())
+
     return jsonify(list_of_friends), 200
 
 @bp.route('/friend_requests', methods=['GET'], strict_slashes=False)
@@ -81,6 +94,7 @@ def get_friend_requests():
     return jsonify(list_of_friend_requests), 200
 
 @bp.route('/sent_friend_requests', methods=['GET'], strict_slashes=False)
+@jwt_required()
 def get_sent_friend_requests():
     user_id = get_jwt_identity()['id']
     sent_friend_requests = Friend.search(user_id=user_id, status="pending")
@@ -92,11 +106,15 @@ def get_sent_friend_requests():
     return jsonify(list_of_sent_friend_requests), 200
 
 @bp.route('/friend/<friend_id>', methods=['GET'], strict_slashes=False)
+@jwt_required()
 def get_friend(friend_id):
     user_id = get_jwt_identity()['id']
     friend = Friend.search(user_id=user_id, friend_id=friend_id)
     if friend is None:
-        return jsonify({'msg': 'Friend not found'}), 404
+        friend = Friend.search(user_id=friend_id, friend_id=user_id)
+        if friend is None:
+            return jsonify({'msg': 'Friend not found'}), 404
+
     return jsonify(friend[0].to_dict()), 200
 
 @bp.route('/block/<friend_id>', methods=['DELETE'], strict_slashes=False)
@@ -104,9 +122,14 @@ def get_friend(friend_id):
 def block_friend(friend_id):
     user_id = get_jwt_identity()['id']
     friend = Friend.search(user_id=user_id, friend_id=friend_id)
+    
     if friend is None:
-        return jsonify({'msg': 'Friend not found'}), 404
-    friend[0].status = "blocked"
-    friend[0].save()
+        friend = Friend.search(friend_id=user_id, user_id=friend_id)
+        if friend is None:
+            return jsonify({'msg': 'Friend not found'}), 404
+    
+    if friend[0].status == "friends":
+        friend[0].status = "blocked"
+        friend[0].save()
 
     return jsonify({"msg": "The friend has been blocked successfully"}), 201
